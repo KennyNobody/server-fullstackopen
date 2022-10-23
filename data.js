78ed73146d5ae0585596e134e20b8d71b3e5c320
Egor Badulin <nobodykenny@gmail.com>
2022-10-23 20:40:24 +0300

    feat: add data

---
+++ data.js
@@ -0,0 +1,29 @@
+const data = [
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    {
+        "name": "Kenny Nobody",
+        "number": "999109109",
+        "id": 5
+    },
+    {
+        "name": "Vasiliy Pupkin",
+        "number": "123123123123",
+        "id": 6
+    },
+    {
+        "name": "Amina Kamalova",
+        "number": "123123-123123-4124-1",
+        "id": 7
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 8
+    }
+];
+
+module.exports.dataList = data;
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const http = require('http')
+const data = require('./data');
 
 const app = http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'text/plain'})
-    response.end('Hello World')
+    response.writeHead(200, {'Content-Type': 'application/json'})
+    response.end(JSON.stringify(data))
 })
 
 const PORT = 3001
