aaf74004a9025741dba93aeab1cb1b3a8b743e47
Egor Badulin <nobodykenny@gmail.com>
2022-10-30 15:35:43 +0300

    feat: complete part 3

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-
 let data = require('./data');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
+
+const Item = require('./models/item');
+
+const errorHandler = (error, request, response, next) => {
+    if (error.name === 'CastError') {
+        return response.status(400).send({error: 'malformatted id'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
+    }
+
+    next(error);
+}
 
 app.use(express.static('build'));
 app.use(cors());
@@ -21,31 +33,12 @@ app.use(morgan((tokens, req, res) => {
     ].join(' ')
 }));
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-});
-
-app.get('/api/persons', (request, response) => {
-    response.json(data)
-});
-
-app.get('/api/info', (request, response) => {
-    const message = `<p>Phonebook has info for ${data.length} people</p>
-                    <p>${new Date}</p>`;
-
-    response.send(message);
-});
-
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+     console.log(`Server running on port ${PORT}`)
 });
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
-});
-
-app.get('/api/persons', (request, response) => {
-    response.json(data)
 });
 
 app.get('/api/info', (request, response) => {
@@ -55,21 +48,44 @@ app.get('/api/info', (request, response) => {
     response.send(message);
 });
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const person = data.find(item => item.id === id);
-
-    if (person) response.json(person);
-    response.status(404).end();
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
 });
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    data = data.filter(item => item.id !== id);
-    response.status(204).end()
+app.get('/api/persons', (request, response) => {
+    Item.find({}).then(note => {
+        response.json(note);
+    });
+});
+
+app.get('/api/info', (request, response) => {
+    const message = `<p>Phonebook has info for ${data.length} people</p>
+                    <p>${new Date}</p>`;
+
+    response.send(message);
+});
+
+app.get('/api/persons/:id', (request, response, next) => {
+    Item.findById(request.params.id)
+        .then(note => {
+            if (note) {
+                response.json(note);
+            } else {
+                response.status(404).end();
+            }
+        })
+        .catch(error => next(error));
+});
+
+app.delete('/api/persons/:id', (request, response, next) => {
+    Item.findByIdAndRemove(request.params.id)
+        .then(() => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body;
 
     if (!body) {
@@ -90,18 +106,16 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    const person = {
+    const person = new Item({
         name: body.name,
         number: body.number,
-        id: generateId(),
-    }
+    });
 
-    data = data.concat(person);
-
-    response.json(person);
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+        })
+        .catch(error => next(error));
 });
 
-const generateId = () => {
-    const maxId = data.length > 0 ? Math.max(...data.map(item => item.id)) : 0;
-    return maxId + 1;
-}
+app.use(errorHandler);
